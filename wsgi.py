c84473892dd0e6de708aa2a65b4e73158b4d06a2
sunghoonism <34115235+sunghoonism@users.noreply.github.com>
2017-12-01 18:51:55 +0900

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,6 @@
-import os
+import uncurl
 
-@application.route("/")
-os.system("curl -X POST https://wh.jandi.com/connect-api/webhook/11495160/86d7ab45df200b89fdedb99158472833 
+print uncurl.parse("curl -X POST https://wh.jandi.com/connect-api/webhook/11495160/86d7ab45df200b89fdedb99158472833 
 -H "Accept: application/vnd.tosslab.jandi-v2+json" 
 -H "Content-Type: application/json" 
 --data-binary '{"body":"You have a new Pizza order.","connectColor"}')
-
-if __name__ == "__main__":
-    application.run()
