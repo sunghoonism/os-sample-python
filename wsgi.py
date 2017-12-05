a8c53079861fb0bbe95ea5a5329ed9eaed22fd4a
sunghoonism <34115235+sunghoonism@users.noreply.github.com>
2017-12-05 11:51:42 +0900

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -6,7 +6,6 @@ headers = {
     'Content-Type': 'application/json',
 }
 data = '{"body":"[[Naver]](http:naver.com) You have a new Pizza order.","connectColor":"#FAC11B","connectInfo":[{"title":"Topping","description":"Pepperoni"},\n{"title":"Location","description":"Empire State Building, 5th Ave, New York","imageUrl":"Url_to_text"}]}'
-
 @application.route("/")
 def hello():
     
