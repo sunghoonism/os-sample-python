9be86493a4d3b65918c77e68af9f0d77f3691b4b
sunghoonism <34115235+sunghoonism@users.noreply.github.com>
2017-12-05 11:52:43 +0900

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,15 +1,8 @@
-from flask import Flask, request
+from flask import Flask
 application = Flask(__name__)
-
-headers = {
-    'Accept': 'application/vnd.tosslab.jandi-v2+json',
-    'Content-Type': 'application/json',
-}
-data = '{"body":"[[Naver]](http:naver.com) You have a new Pizza order.","connectColor":"#FAC11B","connectInfo":[{"title":"Topping","description":"Pepperoni"},\n{"title":"Location","description":"Empire State Building, 5th Ave, New York","imageUrl":"Url_to_text"}]}'
 
 @application.route("/")
 def hello():
-    
     return "Mooooooooii!"
 
 if __name__ == "__main__":
