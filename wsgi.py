2c1cef4f0a4167b956cee863ce50543d3aa288a6
sunghoonism <34115235+sunghoonism@users.noreply.github.com>
2017-12-13 12:59:59 +0900

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -9,7 +9,8 @@ data = '{"body":"[[Naver]](http:naver.com) You have a new Pizza order.","connect
 
 @application.route("/")
 def hello():
-    return "Moooooooii!"
+    requests.post('https://wh.jandi.com/connect-api/webhook/11495160/86d7ab45df200b89fdedb99158472833', headers=headers, data=data)
+    return "Babo"
 
 if __name__ == "__main__":
     application.run()
