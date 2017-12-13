ede4dfa7500e54b60c31769f43fd63dc867eedb1
sunghoonism <34115235+sunghoonism@users.noreply.github.com>
2017-12-13 12:57:30 +0900

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -8,8 +8,8 @@ headers = {
 data = '{"body":"[[Naver]](http:naver.com) You have a new Pizza order.","connectColor":"#FAC11B","connectInfo":[{"title":"Topping","description":"Pepperoni"},\n{"title":"Location","description":"Empire State Building, 5th Ave, New York","imageUrl":"Url_to_text"}]}'
 @application.route("/")
 def hello():
-    
-    return "Mooooooooii!"
+    requests.post('https://wh.jandi.com/connect-api/webhook/11495160/86d7ab45df200b89fdedb99158472833', headers=headers, data=data)
+    return "Babo~"
 
 if __name__ == "__main__":
     application.run()
