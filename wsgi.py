bfd14999bf3fa81d873d44ca20ef2ff1bb5a1448
sunghoonism <34115235+sunghoonism@users.noreply.github.com>
2017-12-19 09:46:37 +0900

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,20 +1,16 @@
-from flask import Flask, request, session
+from flask import Flask, request
 application = Flask(__name__)
 
 headers = {
     'Accept': 'application/vnd.tosslab.jandi-v2+json',
     'Content-Type': 'application/json',
 }
-data = '{"body":"[[Naver]](http:naver.com) You have a new Pizza order.","connectColor":"#FAC11B","connectInfo":[{"title":"Topping","description":"Pepperoni"},\n{"title":"Location","description":"Empire State Building, 5th Ave, New York","imageUrl":"Url_to_text"}]}'
-
+#data = '{"body":"[[Naver]](http:naver.com) You have a new Pizza order.","connectColor":"#FAC11B","connectInfo":[{"title":"Topping","description":"Pepperoni"},\n{"title":"Location","description":"Empire State Building, 5th Ave, New York","imageUrl":"Url_to_text"}]}'
+data = 'from alien'
 @application.route("/")
 def hello():
-    if request.method == 'POST':
-        user_input = request.form['user_input']
-        session['foo'] = 'bar'
-    elif request.method == 'GET':
-        session['foo'] = 'macarena'
-    return session['foo']
+    requests.post('https://wh.jandi.com/connect-api/webhook/11495160/86d7ab45df200b89fdedb99158472833', headers=headers, data=data)
+    return 'foo'
     
 
 if __name__ == "__main__":
