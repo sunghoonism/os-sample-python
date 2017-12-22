c3bc4bcd68ed1315eccb7e5b6f2c1aa1f4b09f0b
sunghoonism <34115235+sunghoonism@users.noreply.github.com>
2017-12-22 11:46:10 +0900

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -9,7 +9,7 @@ headers = {
 #data = 'from alien'
 @application.route("/")
 def hello():
-    return 'Hello'
+    return 'Helloo'
 
 @application.route('/webhook', methods=['GET','POST'])
 def webhook():
