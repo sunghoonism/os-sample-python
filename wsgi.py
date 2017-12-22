7858808e0977b7cbbc19b9b4eb52b9c9823807c6
sunghoonism <34115235+sunghoonism@users.noreply.github.com>
2017-12-22 15:33:37 +0900

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,24 +1,25 @@
 from flask import Flask, request
 application = Flask(__name__)
 
-headers = {
-    'Accept': 'application/vnd.tosslab.jandi-v2+json',
-    'Content-Type': 'application/json',
-}
-#data = '{"body":"[[Naver]](http:naver.com) You have a new Pizza order.","connectColor":"#FAC11B","connectInfo":[{"title":"Topping","description":"Pepperoni"},\n{"title":"Location","description":"Empire State Building, 5th Ave, New York","imageUrl":"Url_to_text"}]}'
-#data = 'from alien'
+
 @application.route("/")
 def hello():
     return 'Hello'
 
 @application.route('/webhook', methods=['POST'])
 def webhook():
+    headers = {
+    'Accept': 'application/vnd.tosslab.jandi-v2+json',
+    'Content-Type': 'application/json'
+    }
+    #data = '{"body":"[[Naver]](http:naver.com) You have a new Pizza order.","connectColor":"#FAC11B","connectInfo":[{"title":"Topping","description":"Pepperoni"},\n{"title":"Location","description":"Empire State Building, 5th Ave, New York","imageUrl":"Url_to_text"}]}'
     #print(request.form['text'])
     #print('POST!')
     #data='{{"text":"{} {}"}}'.format(request.form['text'], request.form['text'])
     data={'body':'I am from webhook'}
-    requests.post('https://wh.jandi.com/connect-api/webhook/11495160/86d7ab45df200b89fdedb99158472833', headers=headers, data=data)
-
+    response=requests.post('https://wh.jandi.com/connect-api/webhook/11495160/86d7ab45df200b89fdedb99158472833', headers=headers, data=data)
+    print(response.text)
+    print(response.status_code, response.reason)
     return 'text'
 
 
