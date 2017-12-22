510f03c59b8bb4c35d726a433edca2b2c855ab5d
sunghoonism <34115235+sunghoonism@users.noreply.github.com>
2017-12-22 17:05:50 +0900

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -8,17 +8,19 @@ def hello():
 
 @application.route('/webhook', methods=['POST'])
 def webhook():
-    headers = {
-    'Accept': 'application/vnd.tosslab.jandi-v2+json',
-    'Content-Type': 'application/json'
-    }
-    #data = '{"body":"[[Naver]](http:naver.com) You have a new Pizza order.","connectColor":"#FAC11B","connectInfo":[{"title":"Topping","description":"Pepperoni"},\n{"title":"Location","description":"Empire State Building, 5th Ave, New York","imageUrl":"Url_to_text"}]}'
-    print(request.form['text'])
-    #print('POST!')
-    data='{{"text":"{} {}"}}'.format(request.form['text'], request.form['text'])
-    #data={'body':'I am from webhook'}
-    #response=requests.post('https://wh.jandi.com/connect-api/webhook/11495160/86d7ab45df200b89fdedb99158472833', headers=headers, data=data)
-    return 'text'
+    if method=='POST':
+        headers = {
+        'Accept': 'application/vnd.tosslab.jandi-v2+json',
+        'Content-Type': 'application/json'
+        }
+        #data = '{"body":"[[Naver]](http:naver.com) You have a new Pizza order.","connectColor":"#FAC11B","connectInfo":[{"title":"Topping","description":"Pepperoni"},\n{"title":"Location","description":"Empire State Building, 5th Ave, New York","imageUrl":"Url_to_text"}]}'
+        print(request.form['text'])
+        #print('POST!')
+        data='{{"text":"{} {}"}}'.format(request.form['text'], request.form['text'])
+        #data={'body':'I am from webhook'}
+        requests.post('https://wh.jandi.com/connect-api/webhook/11495160/86d7ab45df200b89fdedb99158472833', headers=headers, data=data)
+    else:
+        return 'text'
 
 
 if __name__ == "__main__":
